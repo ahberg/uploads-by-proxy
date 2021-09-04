abf9b1d5380e80de9f1f28f894c7cbba7e813402
Anton A <anton.ahrenberg@tidningensyre.se>
2021-09-04 19:20:00 +0200

    Auto format code (PHP Code Beautifier).

--- class-404-template.php
+++ class-404-template.php
@@ -13,7 +13,8 @@ class UBP_404_Template {
 
 	function __construct() {
 		// Only run for whitelisted paths
-		if ( !$this->allow_path() ) { return; }
+		if ( ! $this->allow_path() ) {
+			return; }
 
 		$this->stream();
 	}
@@ -23,31 +24,33 @@ class UBP_404_Template {
 	 */
 	public function stream() {
 
-		$domain =  $this->get_domain() ;
+		$domain = $this->get_domain();
 
 		// Send domain name in request headers so vhosts resolve
 		$args = array( 'headers' => array( 'Host' => $this->get_domain() ) );
 		// Route around local DNS by requesting by IP directly
 		$url = 'http://' . $this->get_auth() . $domain . $this->get_remote_path();
 
-		$this->response = wp_remote_get( $url, $args);
+		$this->response = wp_remote_get( $url, $args );
 
-		if ( !is_wp_error($this->response) && 200 == $this->response['response']['code'] ) {
+		if ( ! is_wp_error( $this->response ) && 200 == $this->response['response']['code'] ) {
 			$this->download();
 		}
 	}
 
 	public function download() {
-		if ( !function_exists('WP_Filesystem')) { require ABSPATH.'wp-admin/includes/file.php'; }
-		global $wp_filesystem; WP_Filesystem();
+		if ( ! function_exists( 'WP_Filesystem' ) ) {
+			require ABSPATH . 'wp-admin/includes/file.php'; }
+		global $wp_filesystem;
+		WP_Filesystem();
 
 		$u = wp_upload_dir();
 
 		$basedir = str_replace( $this->uploads_basedir(), '', $u['basedir'] );
 		$abspath = $basedir . $this->get_local_path();
-		$dir = dirname( $abspath );
+		$dir     = dirname( $abspath );
 
-		if ( !is_dir( $dir ) && !wp_mkdir_p( $dir ) ) {
+		if ( ! is_dir( $dir ) && ! wp_mkdir_p( $dir ) ) {
 			$this->display_and_exit( "Please check permissions. Could not create directory $dir" );
 		}
 
@@ -56,21 +59,22 @@ class UBP_404_Template {
 		if ( $saved_image ) {
 			wp_redirect( get_site_url( get_current_blog_id(), $this->get_local_path() ) );
 			exit;
-		}else {
+		} else {
 			$this->display_and_exit( "Please check permissions. Could not write image $dir" );
 		}
 
 	}
 
-	public function display_and_exit( $message=false ) {
+	public function display_and_exit( $message = false ) {
 		global $wp_query;
 		status_header( 200 );
 		$wp_query->is_404 = false;
 
 		// Send debug message in response headers.
-		if ( $message ) { header('*Uploads-by-Proxy: ' . $message ); }
+		if ( $message ) {
+			header( '*Uploads-by-Proxy: ' . $message ); }
 
-		foreach( $this->response['headers'] as $name => $value ){
+		foreach ( $this->response['headers'] as $name => $value ) {
 			header( "$name: $value" );
 		}
 
@@ -83,7 +87,8 @@ class UBP_404_Template {
 	 */
 	public function allow_path() {
 		$path = $this->get_remote_path();
-		if ( empty( $path ) ) { return false; }
+		if ( empty( $path ) ) {
+			return false; }
 
 		$allowed_paths = array(
 			$this->uploads_basedir(),
@@ -91,8 +96,9 @@ class UBP_404_Template {
 
 		$allowed_paths = apply_filters( 'ubp_allowed_paths', $allowed_paths );
 
-		foreach ( $allowed_paths as $value ){
-			if ( false !== @strpos( $path, $value) ) { return true; }
+		foreach ( $allowed_paths as $value ) {
+			if ( false !== @strpos( $path, $value ) ) {
+				return true; }
 		}
 
 		return false;
@@ -100,6 +106,7 @@ class UBP_404_Template {
 
 	/**
 	 * Return path to uploads folder, relative to WordPress root directory
+	 *
 	 * @var string
 	 */
 	public function uploads_basedir() {
@@ -119,12 +126,12 @@ class UBP_404_Template {
 			$url = str_replace( array( 'http://', 'https://' ), '', UBP_LIVE_DOMAIN );
 			$url = 'http://' . $url;
 
-		}else if ( defined( 'UBP_SITEURL' ) && false !== UBP_SITEURL ) {
-		
+		} elseif ( defined( 'UBP_SITEURL' ) && false !== UBP_SITEURL ) {
+
 			$url = parse_url( UBP_SITEURL );
 			$url = 'http://' . $url['host'] . @$url['path'];
 
-		}else if ( !is_multisite() ) {
+		} elseif ( ! is_multisite() ) {
 			// Nothing set... Get original siteurl from database
 
 			remove_filter( 'option_siteurl', '_config_wp_siteurl' );
@@ -140,23 +147,24 @@ class UBP_404_Template {
 	}
 
 	public function get_domain() {
-		if ( !isset( $this->domain ) ) {
+		if ( ! isset( $this->domain ) ) {
 			$this->domain = parse_url( $this->get_siteurl(), PHP_URL_HOST );
 		}
 		return $this->domain;
 	}
 
 	public function get_auth() {
-		if ( !isset( $this->auth ) ) {
+		if ( ! isset( $this->auth ) ) {
 			$user = parse_url( $this->get_siteurl(), PHP_URL_USER );
 			$pass = parse_url( $this->get_siteurl(), PHP_URL_PASS );
 
-			if ( $user && $pass )
+			if ( $user && $pass ) {
 				$this->auth = $user . ':' . $pass . '@';
-			elseif ( $user )
+			} elseif ( $user ) {
 				$this->auth = $user . '@';
-			else
+			} else {
 				$this->auth = '';
+			}
 		}
 		return $this->auth;
 	}
@@ -168,9 +176,9 @@ class UBP_404_Template {
 
 		// If local install is in a subdirectory, modify path to request from WordPress root
 		$local_wordpress_path = parse_url( get_site_url(), PHP_URL_PATH ) . '/';
-		$requested_path = parse_url( $_SERVER['REQUEST_URI'], PHP_URL_PATH );
-		if (substr($requested_path, 0, strlen($local_wordpress_path)) == $local_wordpress_path) {
-		    $requested_path = substr($requested_path, strlen($local_wordpress_path)-1, strlen($requested_path));
+		$requested_path       = parse_url( $_SERVER['REQUEST_URI'], PHP_URL_PATH );
+		if ( substr( $requested_path, 0, strlen( $local_wordpress_path ) ) == $local_wordpress_path ) {
+			$requested_path = substr( $requested_path, strlen( $local_wordpress_path ) - 1, strlen( $requested_path ) );
 		}
 
 		$this->local_path = $requested_path;
@@ -185,9 +193,9 @@ class UBP_404_Template {
 
 		// If remote install is in a subdirectory, prepend the remote path
 		$remote_path = parse_url( $this->get_siteurl(), PHP_URL_PATH );
-		if ( !empty( $remote_path ) ) {
+		if ( ! empty( $remote_path ) ) {
 			$this->remote_path = $remote_path . $this->get_local_path();
-		}else {
+		} else {
 			$this->remote_path = $this->get_local_path();
 		}
 
--- uploads-by-proxy.php
+++ uploads-by-proxy.php
@@ -17,16 +17,19 @@ Version: 1.1.3
  * this test doesn't return true, redefine it in wp-config.php:
  *     define('UBP_IS_LOCAL', true);
  *
- * 	   WARNING!!
+ *     WARNING!!
  *     Do not set this to "true" on a live site!
  *     Doing so will cause 404 pages for wp-content/uploads to go into
  *     an infinite loop until Apache kills the PHP process.
  */
-if ( !defined('UBP_IS_LOCAL') ) {
-	define('UBP_IS_LOCAL', (
+if ( ! defined( 'UBP_IS_LOCAL' ) ) {
+	define(
+		'UBP_IS_LOCAL',
+		(
 		( '127.0.0.1' == $_SERVER['SERVER_ADDR'] && '127.0.0.1' == $_SERVER['REMOTE_ADDR'] ) // IPv4
 		|| ( '::1' == $_SERVER['SERVER_ADDR'] && '::1' == $_SERVER['REMOTE_ADDR'] ) // IPv6
-	) );
+		)
+	);
 }
 
 /**
@@ -37,17 +40,17 @@ define( 'UBP_PLUGIN_FILE', __FILE__ );
 /**
  * Check for PHP 5.2 or higher before activating.
  */
-if ( version_compare(PHP_VERSION, '5.2', '<') ) {
-	if ( is_admin() && (!defined('DOING_AJAX') || !DOING_AJAX) ) {
-		require_once ABSPATH.'/wp-admin/includes/plugin.php';
+if ( version_compare( PHP_VERSION, '5.2', '<' ) ) {
+	if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
+		require_once ABSPATH . '/wp-admin/includes/plugin.php';
 		deactivate_plugins( UBP_PLUGIN_FILE );
-		wp_die( sprintf( __( 'Uploads by Proxy requires PHP 5.2 or higher, as does WordPress 3.2 and higher. The plugin has now disabled itself. For information on upgrading, %ssee this article%s.', 'uploads-by-proxy'), '<a href="http://codex.wordpress.org/Switching_to_PHP5" target="_blank">', '</a>') );
+		wp_die( sprintf( __( 'Uploads by Proxy requires PHP 5.2 or higher, as does WordPress 3.2 and higher. The plugin has now disabled itself. For information on upgrading, %1$ssee this article%2$s.', 'uploads-by-proxy' ), '<a href="http://codex.wordpress.org/Switching_to_PHP5" target="_blank">', '</a>' ) );
 	} else {
 		return;
 	}
 }
 
-require_once dirname( __FILE__ ).'/class-helpers.php';
+require_once dirname( __FILE__ ) . '/class-helpers.php';
 
 // Only initialize if we're on a development server
 if ( UBP_IS_LOCAL ) {
